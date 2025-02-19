58fc801d7842ba1deb6c2b5c816c387b5d788be4
Lc-am <lucamateo1110@gmail.com>
2025-02-19 14:25:58 +0100

    puerta con codigo + particulas

--- Proyecto/Assets/Systems/SaveGameSystem/Core/Example/Scripts/SaveCubePlayer.cs
+++ Proyecto/Assets/Systems/SaveGameSystem/Core/Example/Scripts/SaveCubePlayer.cs
@@ -1,39 +1,39 @@
-using UnityEditor.Overlays;
-using UnityEngine;
+//using UnityEditor.Overlays;
+//using UnityEngine;
 
-public class SaveCubePlayer : MonoBehaviour
-{
-    GuidComponent guidComponent;
+//public class SaveCubePlayer : MonoBehaviour
+//{
+//    //GuidComponent guidComponent;
 
-    private void Awake()
-    {
-        guidComponent = GetComponent<GuidComponent>();
-    }
+//    //private void Awake()
+//    //{
+//    //    guidComponent = GetComponent<GuidComponent>();
+//    //}
 
-    void Start()
-    {
-        LoadSavedData();
-    }
+//    //void Start()
+//    //{
+//    //    LoadSavedData();
+//    //}
 
-    void Update()
-    {
-        SaveData();
-    }
-    #region SaveGame
-    private void SaveData()
-    {
-        PlayerPrefs.SetFloat(guidComponent.GetGuid() + SaveGameStrings.posicionX, transform.position.x);
-        PlayerPrefs.SetFloat(guidComponent.GetGuid() + SaveGameStrings.posicionY, transform.position.y);
-        PlayerPrefs.SetFloat(guidComponent.GetGuid() + SaveGameStrings.posicionZ, transform.position.z);
-    }
-    private void LoadSavedData()
-    {
-        Vector3 loadedPosition = new Vector3();
-        loadedPosition.x = PlayerPrefs.GetFloat(guidComponent.GetGuid() + SaveGameStrings.posicionX, transform.position.x);
-        loadedPosition.y = PlayerPrefs.GetFloat(guidComponent.GetGuid() + SaveGameStrings.posicionY, transform.position.y);
-        loadedPosition.z = PlayerPrefs.GetFloat(guidComponent.GetGuid() + SaveGameStrings.posicionZ, transform.position.z);
+//    //void Update()
+//    //{
+//    //    SaveData();
+//    //}
+//    //#region SaveGame
+//    //private void SaveData()
+//    //{
+//    //    PlayerPrefs.SetFloat(guidComponent.GetGuid() + SaveGameStrings.posicionX, transform.position.x);
+//    //    PlayerPrefs.SetFloat(guidComponent.GetGuid() + SaveGameStrings.posicionY, transform.position.y);
+//    //    PlayerPrefs.SetFloat(guidComponent.GetGuid() + SaveGameStrings.posicionZ, transform.position.z);
+//    //}
+//    //private void LoadSavedData()
+//    //{
+//    //    Vector3 loadedPosition = new Vector3();
+//    //    loadedPosition.x = PlayerPrefs.GetFloat(guidComponent.GetGuid() + SaveGameStrings.posicionX, transform.position.x);
+//    //    loadedPosition.y = PlayerPrefs.GetFloat(guidComponent.GetGuid() + SaveGameStrings.posicionY, transform.position.y);
+//    //    loadedPosition.z = PlayerPrefs.GetFloat(guidComponent.GetGuid() + SaveGameStrings.posicionZ, transform.position.z);
 
-        transform.position = loadedPosition;
-    }
-    #endregion
-}
+//    //    transform.position = loadedPosition;
+//    //}
+//    //#endregion
+//}
