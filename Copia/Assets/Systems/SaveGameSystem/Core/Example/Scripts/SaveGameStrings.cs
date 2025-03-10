3cf5ca8b4a331b58cb0b38363b823e9c0081a4dd
papirlog <manuel.papirlog@gmail.com>
2025-03-10 14:20:22 +0100

    borrar copia

--- Copia/Assets/Systems/SaveGameSystem/Core/Example/Scripts/SaveGameStrings.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class SaveGameStrings : MonoBehaviour
-{
-    public static string posicionX = "PosicionX";
-    public static string posicionY = "PosicionY";
-    public static string posicionZ = "PosicionZ";
-}
