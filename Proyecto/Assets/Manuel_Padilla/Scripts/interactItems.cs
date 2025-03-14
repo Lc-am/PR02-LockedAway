8910c0e6e7afc1df34afc60fedf30eb0b0125c4f
papirlog <manuel.papirlog@gmail.com>
2025-03-14 11:01:27 +0100

    upgrade interact system, prefabing pipes

--- Proyecto/Assets/Manuel_Padilla/Scripts/interactItems.cs
+++ Proyecto/Assets/Manuel_Padilla/Scripts/interactItems.cs
@@ -1,9 +1,11 @@
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class interactItems : NetworkBehaviour
 {
+    [Header("Interactions")]
     [SerializeField] private InputActionReference interact;
     [SerializeField] private float interactRange = 5f;
     [SerializeField] private Transform camera;
@@ -24,21 +26,18 @@ public class interactItems : NetworkBehaviour
 
     private void OnInteract(InputAction.CallbackContext context)
     {
+        Debug.Log("Interacting");
         //Funcion que lanza un raycast cada vez que se pulsa el boton de interacción para saber si puede y que puede hacer el objeto interactuado.
-        if(IsLocalPlayer)
+        if (IsLocalPlayer)
         {
             RaycastHit hit;
+            Debug.Log("Interacting");
 
             if (Physics.Raycast(camera.position, camera.TransformDirection(Vector3.forward), out hit, interactRange))
             {
-                if (hit.transform.CompareTag("Door"))
-                {
-                    doorInteract door = hit.transform.GetComponent<doorInteract>();
-                    if (door != null)
-                    {
-                        door.Interact();
-                    }
-                }
+                IInteractable interactable = hit.transform.GetComponent<IInteractable>();
+                if (interactable != null)
+                    { interactable.StartInteraction(); }
             }
         }
     }
