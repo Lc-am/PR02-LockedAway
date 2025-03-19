a5e20b9381c443a9bb39e483183c5dfe756d2499
papirlog <manuel.papirlog@gmail.com>
2025-03-19 12:43:24 +0100

    pipes prefabs and controller

--- Proyecto/Assets/Systems/Puzzles/Pipes/Scripts/pipeController.cs
+++ Proyecto/Assets/Systems/Puzzles/Pipes/Scripts/pipeController.cs
@@ -4,38 +4,19 @@ using UnityEngine.Events;
 
 public class pipeController : NetworkBehaviour, IInteractable
 {
-    [SerializeField] private int correctState;
-    private int currentState = 0;
-
-    [SerializeField] private Vector3 starterRotation;   //poner la rotacion inicial
     [SerializeField] private Vector3 neededRotation;    //poner la suma que se quiere rotar
 
     private Quaternion rotationToAdd;
 
     private void Awake()
     {
-        transform.rotation = Quaternion.Euler(starterRotation);
+        //transform.rotation = Quaternion.Euler(starterRotation);
         rotationToAdd = Quaternion.Euler(neededRotation);
     }
 
     public void pipeRotate()
     {
-        Debug.Log("Rotating pipe");
         transform.rotation *= rotationToAdd;    //Añade la rotacion puesta a la rotacion actual
-
-        if (currentState <=3)   //Actualiza el estado para revisar si esta bien o no
-        {
-            currentState++;
-        }
-        else
-        {
-            currentState = 0;
-        }
-
-        if(currentState == correctState)
-        {
-            Debug.Log("Pipe is correct");
-        }
     }
 
     void IInteractable.StartInteraction()
