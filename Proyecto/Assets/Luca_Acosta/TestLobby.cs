c84649f18bbe5e555400bbfe2238ffea2eb16a0c
Lc-am <lucamateo1110@gmail.com>
2025-03-26 12:34:44 +0100

    script lobby cambios

--- Proyecto/Assets/Luca_Acosta/TestLobby.cs
+++ Proyecto/Assets/Luca_Acosta/TestLobby.cs
@@ -2,36 +2,76 @@ using UnityEngine;
 using Unity.Services.Core;
 using Unity.Services.Authentication;
 using UnityEngine.SceneManagement;
+using Unity.Services.Lobbies.Models;
+using Unity.Services.Lobbies;
+
+
 
 public class TestLobby : MonoBehaviour
 {
+    //async void Start()
+    //{
+    //    await UnityServices.InitializeAsync();
+        
+    //    if(UnityServices.State == ServicesInitializationState.Initialized)
+    //    {
+    //        AuthenticationService.Instance.SignedIn += OnSignedIn;
+
+    //        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+    //        if(AuthenticationService.Instance.IsSignedIn)
+    //        {
+    //            string username = PlayerPrefs.GetString(key: "Username");
+    //            if(username == "")
+    //            {
+    //                username = "Player";
+    //                PlayerPrefs.GetString("Username", username);
+    //            }
+    //        }
+
+    //        SceneManager.LoadSceneAsync("Splash Company");
+    //    }
+    //}
+
+    //private void OnSignedIn()
+    //{
+    //    Debug.Log(message: $"Player Id:  {AuthenticationService.Instance.PlayerId}");
+    //    Debug.Log(message: $"Token:  {AuthenticationService.Instance.AccessToken}");
+    //}
+
     async void Start()
     {
-        await UnityServices.InitializeAsync();
-        
-        if(UnityServices.State == ServicesInitializationState.Initialized)
+        await  UnityServices.InitializeAsync();
+        AuthenticationService.Instance.SignedIn += ()
+            =>
         {
-            AuthenticationService.Instance.SignedIn += OnSignedIn;
+            Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);
+        };
 
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
 
-            if(AuthenticationService.Instance.IsSignedIn)
-            {
-                string username = PlayerPrefs.GetString(key: "Username");
-                if(username == "")
-                {
-                    username = "Player";
-                    PlayerPrefs.GetString("Username", username);
-                }
-            }
+    private async void CreateLobby()
+    {
+        try
+        {
+            string lobbyName = "myLobby";
+            int maxPlayers = 4;
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
 
-            SceneManager.LoadSceneAsync("Splash Company");
+            Debug.Log("Created Lobby! " + lobby.Name + " " + lobby.MaxPlayers);
+        }
+        catch(LobbyServiceException e)
+        {
+            Debug.Log(e);
         }
     }
 
-    private void OnSignedIn()
-    {
-        Debug.Log(message: $"Player Id:  {AuthenticationService.Instance.PlayerId}");
-        Debug.Log(message: $"Token:  {AuthenticationService.Instance.AccessToken}");
-    }
+    //private async void ListLobbies()
+    //{
+    //    try
+    //    {
+             
+    //    }
+    //}
 }
