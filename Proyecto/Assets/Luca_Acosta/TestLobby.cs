87684fd2817d1c98ed40692d380681a663b438eb
Lc-am <lucamateo1110@gmail.com>
2025-03-26 14:14:04 +0100

    lobby

--- Proyecto/Assets/Luca_Acosta/TestLobby.cs
+++ Proyecto/Assets/Luca_Acosta/TestLobby.cs
@@ -4,6 +4,7 @@ using Unity.Services.Authentication;
 using UnityEngine.SceneManagement;
 using Unity.Services.Lobbies.Models;
 using Unity.Services.Lobbies;
+using System.Collections.Generic;
 
 
 
@@ -12,7 +13,7 @@ public class TestLobby : MonoBehaviour
     //async void Start()
     //{
     //    await UnityServices.InitializeAsync();
-        
+
     //    if(UnityServices.State == ServicesInitializationState.Initialized)
     //    {
     //        AuthenticationService.Instance.SignedIn += OnSignedIn;
@@ -39,11 +40,13 @@ public class TestLobby : MonoBehaviour
     //    Debug.Log(message: $"Token:  {AuthenticationService.Instance.AccessToken}");
     //}
 
+    private Lobby hostLobby;
+    private float heartbeatTimer;
+
     async void Start()
     {
         await  UnityServices.InitializeAsync();
-        AuthenticationService.Instance.SignedIn += ()
-            =>
+        AuthenticationService.Instance.SignedIn += () =>
         {
             Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);
         };
@@ -51,13 +54,34 @@ public class TestLobby : MonoBehaviour
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
-    private async void CreateLobby()
+    private void Update()
     {
+        HandleLobbyHeartbeat();
+    }
+
+    async void HandleLobbyHeartbeat()
+    {
+        if (hostLobby != null)
+        {
+            heartbeatTimer -= Time.deltaTime;
+            if(heartbeatTimer < 0f)
+            {
+                float heartbeatTimerMax = 15f;
+                heartbeatTimer = heartbeatTimerMax;
+
+                await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+            }
+        }
+    }
+
+    async void CreateLobby()
+     {
         try
         {
             string lobbyName = "myLobby";
             int maxPlayers = 4;
             Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
+            hostLobby = lobby;
 
             Debug.Log("Created Lobby! " + lobby.Name + " " + lobby.MaxPlayers);
         }
@@ -65,13 +89,51 @@ public class TestLobby : MonoBehaviour
         {
             Debug.Log(e);
         }
+     }
+
+    private async void ListLobbies()
+    {
+        try 
+        {
+            QueryLobbiesOptions queryLobbiesOptions = new QueryLobbiesOptions
+            {
+                Count = 25,
+                Filters = new List<QueryFilter>
+                {
+                    new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT)
+                },
+                Order = new List<QueryOrder>
+                {
+                    new QueryOrder(false, QueryOrder.FieldOptions.Created)
+                }
+            };
+            
+            
+            QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync();
+
+            Debug.Log("Lobbies found: " + queryResponse.Results.Count);
+            foreach (Lobby lobby in queryResponse.Results)
+            {
+                Debug.Log(lobby.Name + " " + lobby.MaxPlayers) ;
+            }
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.LogError(e);
+        }
     }
 
-    //private async void ListLobbies()
-    //{
-    //    try
-    //    {
-             
-    //    }
-    //}
+    private async void JoinLobby()
+    {
+        try
+        {
+            QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync();
+
+            await LobbyService.Instance.JoinLobbyByIdAsync(queryResponse.Results[0].Id);
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.LogError(e);
+        }
+    }
 }
