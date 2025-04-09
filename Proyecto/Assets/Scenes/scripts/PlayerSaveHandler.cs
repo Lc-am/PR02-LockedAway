6efbd465f1a0ececd5c2a86dc0924831f8d34d6c
Lc-am <lucamateo1110@gmail.com>
2025-04-09 13:10:19 +0200

    menu audio

--- Proyecto/Assets/Scenes/scripts/PlayerSaveHandler.cs
+++ Proyecto/Assets/Scenes/scripts/PlayerSaveHandler.cs
@@ -1,66 +1,66 @@
-using UnityEngine;
-using Unity.Netcode;
+//using UnityEngine;
+//using Unity.Netcode;
 
-public class PlayerSaveHandler : NetworkBehaviour
-{
-    private PlayerControllerNetwork player;
+//public class PlayerSaveHandler : NetworkBehaviour
+//{
+//    private PlayerControllerNetwork player;
 
-    private void Awake()
-    {
-        player = GetComponent<PlayerControllerNetwork>();
-    }
+//    private void Awake()
+//    {
+//        player = GetComponent<PlayerControllerNetwork>();
+//    }
 
-    public override void OnNetworkSpawn()
-    {
-        if (IsLocalPlayer)
-        {
-            LoadPlayerData(); // Cargar datos solo para el jugador local al entrar en la red
-        }
-    }
+//    public override void OnNetworkSpawn()
+//    {
+//        if (IsLocalPlayer)
+//        {
+//            LoadPlayerData(); // Cargar datos solo para el jugador local al entrar en la red
+//        }
+//    }
 
-    private void Update()
-    {
-        if (!IsLocalPlayer) return; // Asegurarse de que solo el jugador local pueda guardar
+//    private void Update()
+//    {
+//        if (!IsLocalPlayer) return; // Asegurarse de que solo el jugador local pueda guardar
 
-        // Guarda los datos del jugador al pulsar la tecla 'J'
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            SavePlayerData();
-        }
-    }
+//        // Guarda los datos del jugador al pulsar la tecla 'J'
+//        if (Input.GetKeyDown(KeyCode.J))
+//        {
+//            SavePlayerData();
+//        }
+//    }
 
-    private void OnApplicationQuit()
-    {
-        if (IsLocalPlayer)
-        {
-            SaveGameSystem.Save(); // Guarda datos al cerrar la aplicación
-        }
-    }
+//    private void OnApplicationQuit()
+//    {
+//        if (IsLocalPlayer)
+//        {
+//            SaveGameSystem.Save(); // Guarda datos al cerrar la aplicación
+//        }
+//    }
 
-    private void SavePlayerData()
-    {
-        // Guarda la posición, rotación y estado agachado del jugador
-        SaveGameSystem.SetFloat("Player_Pos_X", transform.position.x);
-        SaveGameSystem.SetFloat("Player_Pos_Y", transform.position.y);
-        SaveGameSystem.SetFloat("Player_Pos_Z", transform.position.z);
-        SaveGameSystem.SetFloat("RotationX", player.GetRotationX());
-        SaveGameSystem.SetBool("IsCrouched", player.IsCrouched());
-        SaveGameSystem.Save();
-        Debug.Log("Datos del jugador guardados.");
-    }
+//    private void SavePlayerData()
+//    {
+//        // Guarda la posición, rotación y estado agachado del jugador
+//        SaveGameSystem.SetFloat("Player_Pos_X", transform.position.x);
+//        SaveGameSystem.SetFloat("Player_Pos_Y", transform.position.y);
+//        SaveGameSystem.SetFloat("Player_Pos_Z", transform.position.z);
+//        SaveGameSystem.SetFloat("RotationX", player.GetRotationX());
+//        SaveGameSystem.SetBool("IsCrouched", player.IsCrouched());
+//        SaveGameSystem.Save();
+//        Debug.Log("Datos del jugador guardados.");
+//    }
 
-    private void LoadPlayerData()
-    {
-        // Cargar la posición, rotación y estado agachado del jugador
-        float x = SaveGameSystem.GetFloat("Player_Pos_X", transform.position.x);
-        float y = SaveGameSystem.GetFloat("Player_Pos_Y", transform.position.y);
-        float z = SaveGameSystem.GetFloat("Player_Pos_Z", transform.position.z);
-        transform.position = new Vector3(x, y, z);
+//    private void LoadPlayerData()
+//    {
+//        // Cargar la posición, rotación y estado agachado del jugador
+//        float x = SaveGameSystem.GetFloat("Player_Pos_X", transform.position.x);
+//        float y = SaveGameSystem.GetFloat("Player_Pos_Y", transform.position.y);
+//        float z = SaveGameSystem.GetFloat("Player_Pos_Z", transform.position.z);
+//        transform.position = new Vector3(x, y, z);
 
-        float rotX = SaveGameSystem.GetFloat("RotationX", 0f);
-        player.SetRotationX(rotX);
+//        float rotX = SaveGameSystem.GetFloat("RotationX", 0f);
+//        player.SetRotationX(rotX);
 
-        bool wasCrouched = SaveGameSystem.GetBool("IsCrouched", false);
-        player.SetCrouchState(wasCrouched);
-    }
-}
+//        bool wasCrouched = SaveGameSystem.GetBool("IsCrouched", false);
+//        player.SetCrouchState(wasCrouched);
+//    }
+//}
--- Proyecto/Assets/Scenes/scripts/SaveGameSystem.cs
+++ Proyecto/Assets/Scenes/scripts/SaveGameSystem.cs
@@ -1,210 +1,210 @@
-using System.Collections.Generic;
-using UnityEditor.Overlays;
-using UnityEngine;
-using UnityEditor;
-using System;
+//using System.Collections.Generic;
+//using UnityEditor.Overlays;
+//using UnityEngine;
+//using UnityEditor;
+//using System;
 
-public static class SaveGameSystem
-{
-    [System.Serializable]
-    public class Data
-    {
-        public enum DataType
-        {
-            Int,
-            Float,
-            String,
-            Bool
-        }
+//public static class SaveGameSystem
+//{
+//    [System.Serializable]
+//    public class Data
+//    {
+//        public enum DataType
+//        {
+//            Int,
+//            Float,
+//            String,
+//            Bool
+//        }
 
-        public DataType dataType;
+//        public DataType dataType;
 
-        public int intData;
-        public float floatData;
-        public string stringData;
-        public bool boolData;
-    }
+//        public int intData;
+//        public float floatData;
+//        public string stringData;
+//        public bool boolData;
+//    }
 
-    static Dictionary<string, Data> savedData = new();
+//    static Dictionary<string, Data> savedData = new();
 
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    static void OnRuntimeMethodLoad()
-    {
-        Load();
-    }
+//    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+//    static void OnRuntimeMethodLoad()
+//    {
+//        Load();
+//    }
 
-    public static int GetInt(string key, int defaultValue = 0)
-    {
-        int retVal = defaultValue;
-        if (savedData.ContainsKey(key))
-        {
-            // CheckDataIsInt();
-            retVal = savedData[key].intData;
-        }
-        return retVal;
-    }
+//    public static int GetInt(string key, int defaultValue = 0)
+//    {
+//        int retVal = defaultValue;
+//        if (savedData.ContainsKey(key))
+//        {
+//            // CheckDataIsInt();
+//            retVal = savedData[key].intData;
+//        }
+//        return retVal;
+//    }
 
-    public static float GetFloat(string key, float defaultValue = 0f)
-    {
-        float retVal = defaultValue;
-        if (savedData.ContainsKey(key))
-        {
-            // CheckDataIsInt();
-            retVal = savedData[key].floatData;
-        }
-        return retVal;
-    }
+//    public static float GetFloat(string key, float defaultValue = 0f)
+//    {
+//        float retVal = defaultValue;
+//        if (savedData.ContainsKey(key))
+//        {
+//            // CheckDataIsInt();
+//            retVal = savedData[key].floatData;
+//        }
+//        return retVal;
+//    }
 
-    public static string GetString(string key, string defaultValue = "")
-    {
-        string retVal = defaultValue;
-        if (savedData.ContainsKey(key))
-        {
-            // CheckDataIsInt();
-            retVal = savedData[key].stringData;
-        }
-        return retVal;
-    }
+//    public static string GetString(string key, string defaultValue = "")
+//    {
+//        string retVal = defaultValue;
+//        if (savedData.ContainsKey(key))
+//        {
+//            // CheckDataIsInt();
+//            retVal = savedData[key].stringData;
+//        }
+//        return retVal;
+//    }
 
-    public static bool GetBool(string key, bool defaultValue = false)
-    {
-        bool retVal = defaultValue;
-        if (savedData.ContainsKey(key))
-        {
-            // CheckDataIsInt();
-            retVal = savedData[key].boolData;
-        }
-        return retVal;
-    }
+//    public static bool GetBool(string key, bool defaultValue = false)
+//    {
+//        bool retVal = defaultValue;
+//        if (savedData.ContainsKey(key))
+//        {
+//            // CheckDataIsInt();
+//            retVal = savedData[key].boolData;
+//        }
+//        return retVal;
+//    }
 
 
 
-    public static void SetInt(string key, int value)
-    {
-        Data data;
+//    public static void SetInt(string key, int value)
+//    {
+//        Data data;
 
-        if (!savedData.TryGetValue(key, out data))
-        {
-            data = new Data();
-            data.dataType = Data.DataType.Int;
-            savedData.Add(key, data);
-        }
+//        if (!savedData.TryGetValue(key, out data))
+//        {
+//            data = new Data();
+//            data.dataType = Data.DataType.Int;
+//            savedData.Add(key, data);
+//        }
 
-        data.intData = value;
-    }
+//        data.intData = value;
+//    }
 
-    public static void SetFloat(string key, float value)
-    {
-        Data data;
+//    public static void SetFloat(string key, float value)
+//    {
+//        Data data;
 
-        if (!savedData.TryGetValue(key, out data))
-        {
-            data = new Data();
-            data.dataType = Data.DataType.Float;
-            savedData.Add(key, data);
-        }
+//        if (!savedData.TryGetValue(key, out data))
+//        {
+//            data = new Data();
+//            data.dataType = Data.DataType.Float;
+//            savedData.Add(key, data);
+//        }
 
-        data.floatData = value;
-    }
-    public static void SetString(string key, string value)
-    {
-        Data data;
+//        data.floatData = value;
+//    }
+//    public static void SetString(string key, string value)
+//    {
+//        Data data;
 
-        if (!savedData.TryGetValue(key, out data))
-        {
-            data = new Data();
-            data.dataType = Data.DataType.String;
-            savedData.Add(key, data);
-        }
+//        if (!savedData.TryGetValue(key, out data))
+//        {
+//            data = new Data();
+//            data.dataType = Data.DataType.String;
+//            savedData.Add(key, data);
+//        }
 
-        data.stringData = value;
-    }
-    public static void SetBool(string key, bool value)
-    {
-        Data data;
+//        data.stringData = value;
+//    }
+//    public static void SetBool(string key, bool value)
+//    {
+//        Data data;
 
-        if (!savedData.TryGetValue(key, out data))
-        {
-            data = new Data();
-            data.dataType = Data.DataType.Bool;
-            savedData.Add(key, data);
-        }
+//        if (!savedData.TryGetValue(key, out data))
+//        {
+//            data = new Data();
+//            data.dataType = Data.DataType.Bool;
+//            savedData.Add(key, data);
+//        }
 
-        data.boolData = value;
-    }
+//        data.boolData = value;
+//    }
 
-    [System.Serializable] class SerializableData
-    {
-        public List<string> keys = new();
-        public List<Data> data = new();
-    }
+//    [System.Serializable] class SerializableData
+//    {
+//        public List<string> keys = new();
+//        public List<Data> data = new();
+//    }
 
-    public static void Save(string saveGameName = "SaveGame")
-    {
-        SerializableData serializableData = new();
+//    public static void Save(string saveGameName = "SaveGame")
+//    {
+//        SerializableData serializableData = new();
 
-        foreach (KeyValuePair<string, Data> item in savedData)
-        {
-            serializableData.keys.Add(item.Key);
-            serializableData.data.Add(item.Value);
-        }
+//        foreach (KeyValuePair<string, Data> item in savedData)
+//        {
+//            serializableData.keys.Add(item.Key);
+//            serializableData.data.Add(item.Value);
+//        }
 
-        string stringToSave = JsonUtility.ToJson(serializableData);
-        Debug.Log(stringToSave);
+//        string stringToSave = JsonUtility.ToJson(serializableData);
+//        Debug.Log(stringToSave);
 
-        PlayerPrefs.SetString(saveGameName, stringToSave);
-        PlayerPrefs.Save();
-    }
+//        PlayerPrefs.SetString(saveGameName, stringToSave);
+//        PlayerPrefs.Save();
+//    }
 
-    public static void Load(string saveGameName = "SaveGame")
-    {
-        string stringToLoad = PlayerPrefs.GetString(saveGameName);
+//    public static void Load(string saveGameName = "SaveGame")
+//    {
+//        string stringToLoad = PlayerPrefs.GetString(saveGameName);
 
-        SerializableData serializableData = new();
-        JsonUtility.FromJsonOverwrite(stringToLoad, serializableData);
+//        SerializableData serializableData = new();
+//        JsonUtility.FromJsonOverwrite(stringToLoad, serializableData);
 
-        savedData = new();
-        for (int i = 0; i < serializableData.keys.Count; i++)
-        {
-            savedData.Add(
-                serializableData.keys[i],
-                serializableData.data[i]
-                );
-        }
-    }
+//        savedData = new();
+//        for (int i = 0; i < serializableData.keys.Count; i++)
+//        {
+//            savedData.Add(
+//                serializableData.keys[i],
+//                serializableData.data[i]
+//                );
+//        }
+//    }
 
-#if UNITY_EDITOR
+//#if UNITY_EDITOR
 
-    [MenuItem("SaveGameSystem/Save")]
-    public static void SaveToPlayerPrefs()
-    {
-        SaveGameSystem.Save();
-    }
+//    [MenuItem("SaveGameSystem/Save")]
+//    public static void SaveToPlayerPrefs()
+//    {
+//        SaveGameSystem.Save();
+//    }
 
-    [MenuItem("SaveGameSystem/Load")]
-    public static void LoadFromPlayerPrefs()
-    {
-        SaveGameSystem.Load();
-    }
+//    [MenuItem("SaveGameSystem/Load")]
+//    public static void LoadFromPlayerPrefs()
+//    {
+//        SaveGameSystem.Load();
+//    }
 
-    static string testKey = "RandomInt";
-    [MenuItem("SaveGameSystem/Save int to 5")]
-    public static void SaveIntTo5()
-    {
-        SaveGameSystem.SetInt(testKey, 5);
-    }
+//    static string testKey = "RandomInt";
+//    [MenuItem("SaveGameSystem/Save int to 5")]
+//    public static void SaveIntTo5()
+//    {
+//        SaveGameSystem.SetInt(testKey, 5);
+//    }
 
-    [MenuItem("SaveGameSystem/Save int to 7")]
-    public static void SaveIntTo7()
-    {
-        SaveGameSystem.SetInt(testKey, 7);
-    }
+//    [MenuItem("SaveGameSystem/Save int to 7")]
+//    public static void SaveIntTo7()
+//    {
+//        SaveGameSystem.SetInt(testKey, 7);
+//    }
 
-    [MenuItem("SaveGameSystem/Debug log int")]
-    public static void DebugLogInt()
-    {
-        Debug.Log(SaveGameSystem.GetInt(testKey));
-    }
-#endif
+//    [MenuItem("SaveGameSystem/Debug log int")]
+//    public static void DebugLogInt()
+//    {
+//        Debug.Log(SaveGameSystem.GetInt(testKey));
+//    }
+//#endif
 
-}
+//}
