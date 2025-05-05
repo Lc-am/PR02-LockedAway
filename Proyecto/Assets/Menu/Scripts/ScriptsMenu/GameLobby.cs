f6f74ddbf2f6607199f751305fa3c6757fec18bf
papirlog <manuel.papirlog@gmail.com>
2025-05-05 10:24:58 +0200

    Update GameLobby.cs

--- Proyecto/Assets/Menu/Scripts/ScriptsMenu/GameLobby.cs
+++ Proyecto/Assets/Menu/Scripts/ScriptsMenu/GameLobby.cs
@@ -10,8 +10,20 @@ public class LobbyManager : NetworkBehaviour
     [SerializeField] private Button startGameButton;
     [SerializeField] private string GameScene;
 
+    private void OnEnable()
+    {
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+    }
+
+    private void OnDisable()
+    {
+        NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+    }
+
     private void Start()
     {
+        UpdatePlayerListUI();
+
         if (NetworkManager.Singleton.IsHost)
         {
             startGameButton.gameObject.SetActive(true);
@@ -21,10 +33,16 @@ public class LobbyManager : NetworkBehaviour
         {
             startGameButton.gameObject.SetActive(false);
         }
+    }
 
+    private void OnClientConnected(ulong clientId)
+    {
         UpdatePlayerListUI();
-        NetworkManager.Singleton.OnClientConnectedCallback += (_) => UpdatePlayerListUI();
-        NetworkManager.Singleton.OnClientDisconnectCallback += (_) => UpdatePlayerListUI();
+
+        if (NetworkManager.Singleton.IsHost)
+        {
+            startGameButton.gameObject.SetActive(true);
+        }
     }
 
     void UpdatePlayerListUI()
@@ -46,3 +64,75 @@ public class LobbyManager : NetworkBehaviour
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using Unity.Netcode;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using TMPro;
+//using UnityEngine.SceneManagement;
+
+//public class LobbyManager : NetworkBehaviour
+//{
+//    [SerializeField] private TMP_Text playersListText;
+//    [SerializeField] private Button startGameButton;
+//    [SerializeField] private string GameScene;
+
+//    private void Start()
+//    {
+//        if (NetworkManager.Singleton.IsHost)
+//        {
+//            startGameButton.gameObject.SetActive(true);
+//            startGameButton.onClick.AddListener(TryStartGame);
+//        }
+//        else
+//        {
+//            startGameButton.gameObject.SetActive(false);
+//        }
+
+//        UpdatePlayerListUI();
+//        NetworkManager.Singleton.OnClientConnectedCallback += (_) => UpdatePlayerListUI();
+//        NetworkManager.Singleton.OnClientDisconnectCallback += (_) => UpdatePlayerListUI();
+//    }
+
+//    void UpdatePlayerListUI()
+//    {
+//        string playerList = "Jugadores conectados:\n";
+//        foreach (var client in NetworkManager.Singleton.ConnectedClientsList)
+//        {
+//            playerList += $"- Jugador {client.ClientId}\n";
+//        }
+
+//        playersListText.text = playerList;
+//    }
+
+//    void TryStartGame()
+//    {
+//        if (NetworkManager.Singleton.ConnectedClients.Count == 2)
+//        {
+//            NetworkManager.Singleton.SceneManager.LoadScene(GameScene, LoadSceneMode.Single);
+//        }
+//    }
+//}
