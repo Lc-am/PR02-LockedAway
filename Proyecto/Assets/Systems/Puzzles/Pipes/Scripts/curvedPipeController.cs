652723e20fb2f52f17f5a00032a8da332648176b
papirlog <manuel.papirlog@gmail.com>
2025-05-07 13:46:56 +0200

    Update curvedPipeController.cs

--- Proyecto/Assets/Systems/Puzzles/Pipes/Scripts/curvedPipeController.cs
+++ Proyecto/Assets/Systems/Puzzles/Pipes/Scripts/curvedPipeController.cs
@@ -14,6 +14,7 @@ public class curvedPipeController : MonoBehaviour, IInteractable
 
     private void pipeRotate()
     {
+        Debug.Log("rotate");
         if (rotated)
         {
             transform.rotation = Quaternion.Euler(pipeRotation2);
